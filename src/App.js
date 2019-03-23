e6534a0be5a811232ccd85823e6bd9d9dbce7ad1
Naomi Jacobs <naomi@benchling.com>
2019-03-23 13:25:01 -0700

    Reset to empty hook state
    Also change theme to use colors, fix spacing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 
-import { NameWithHooks, NameWithoutHooks } from "./Name";
+import { NameWithHooks, NameWithClass } from "./Name";
 
 function App() {
-  const [showHooks, setShowHooks] = useState(false);
-
   return (
     <div className="App">
       <header className="App-header">
-        <span className={"toggleHooks"}>
-          <button type="checkbox" onClick={() => setShowHooks(!showHooks)}>
-            {showHooks ? "HIDE HOOKS" : "SHOW HOOKS"}
-          </button>
-        </span>
         <div className="hooks">
-          <NameWithoutHooks />
-          {showHooks && <NameWithHooks />}
+          <NameWithClass />
+          <NameWithHooks />
         </div>
       </header>
     </div>
--- src/Name.jsx
+++ src/Name.jsx
@@ -1,100 +1,55 @@
-import React, {useState, useEffect} from 'react';
+import React from "react";
 
-export class NameWithoutHooks extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: 'plasmid001',
-            bases: 'GATTACA',
-            width: window.innerWidth,
-        };
+export class NameWithClass extends React.Component {
+  state = {
+    name: "Naomi",
+    favoriteColor: "teal",
+    width: window.innerWidth
+  };
 
-        this.handleResize = this.handleResize.bind(this);
-        this.updateBases = this.updateBases.bind(this);
-        this.updateName = this.updateName.bind(this);
-    }
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+    document.title = this.state.name;
+  }
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleResize);
-        document.title = this.state.name;
-    }
+  componentDidUpdate() {
+    document.title = this.state.name;
+  }
 
-    componentDidUpdate() {
-        document.title = this.state.name;
-    }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize);
-    }
+  handleResize = () => this.setState({ width: window.innerWidth });
 
-    handleResize() {
-        this.setState({width: window.innerWidth});
-    }
+  updateName = e => this.setState({ name: e.target.value });
 
-    updateName(e) {
-        this.setState({name: e.target.value});
-    }
+  updateFavoriteColor = e => this.setState({ favoriteColor: e.target.value });
 
-    updateBases(e) {
-        this.setState({bases: e.target.value});
-    }
-
-    render() {
-        return <div className="demo withoutHooks">
-            <div className="title">With Classes</div>
-            <div>
-                Name:
-                <input value={this.state.name} onChange={this.updateName} />
-            </div>
-            <div>
-                Bases:
-                <input value={this.state.bases} onChange={this.updateBases} />
-            </div>
-            <div>Width: {this.state.width}</div>
-        </div>;
-    }
-}
-
-function useFormInput(initialValue) {
-    const [value, setValue] = useState(initialValue);
-    const onChange = (e) => setValue(e.target.value);
-
-    return {value, onChange};
-}
-
-function useWindowWidth() {
-    const [width, setWidth] = useState(window.innerWidth);
-    const handleResize = () => setWidth(window.innerWidth);
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            console.log(width);
-        }
-    }, []);
-
-    return width;
+  render() {
+    return (
+      <div
+        className="demo withoutHooks"
+        style={{ backgroundColor: this.state.favoriteColor }}
+      >
+        <div className="title">With Classes</div>
+        <div className="inputWrapper">
+          Name:
+          <input value={this.state.name} onChange={this.updateName} />
+        </div>
+        <div className="inputWrapper">
+          Fave Color:
+          <input
+            value={this.state.favoriteColor}
+            onChange={this.updateFavoriteColor}
+          />
+        </div>
+        <div>Width: {this.state.width}</div>
+      </div>
+    );
+  }
 }
 
 export function NameWithHooks() {
-    const name = useFormInput('plasmid001');
-    const bases = useFormInput('GATTACA');
-    const width = useWindowWidth();
-
-    useEffect(() => {
-        document.title = name.value;
-    }, [name]);
-
-    return <div className="demo withHooks">
-        <div className="title">With Hooks</div>
-        <div>
-            Name:
-            <input {...name} />
-        </div>
-        <div>
-            Bases:
-            <input {...bases} />
-        </div>
-        <div>Width: {width}</div>
-    </div>;
+  return <div />;
 }
