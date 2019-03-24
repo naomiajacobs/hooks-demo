7d7a449bc8cb6d78a6d736c5956adc230734cbea
Naomi Jacobs <naomi@benchling.com>
2019-03-23 17:10:28 -0700

    With hooks, no custom hooks

--- src/Name.jsx
+++ src/Name.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export class NameWithClass extends React.Component {
   state = {
@@ -51,5 +51,35 @@ export class NameWithClass extends React.Component {
 }
 
 export function NameWithHooks() {
-  return <div />;
+  const [name, setName] = useState("Naomi");
+  const updateName = e => setName(e.target.value);
+  useEffect(() => (document.title = name));
+
+  const [favoriteColor, setFavoriteColor] = useState("teal");
+  const updateFavoriteColor = e => setFavoriteColor(e.target.value);
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleResize = () => setWidth(window.innerWidth);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // second parameter is optional, would be fine without it
+
+  return (
+    <div
+      className="demo withHooks"
+      style={{ backgroundColor: favoriteColor }}
+    >
+      <div className="title">With Classes</div>
+      <div className="inputWrapper">
+        Name:
+        <input value={name} onChange={updateName} />
+      </div>
+      <div className="inputWrapper">
+        Fave Color:
+        <input value={favoriteColor} onChange={updateFavoriteColor} />
+      </div>
+      <div>Width: {width}</div>
+    </div>
+  );
 }
